df11dc16093f33d3d595bb64edefe036163f2033
Dmitrii Parfenov <parfenovds@protonmail.com>
2023-03-01 09:30:02 +0300

    style refactoring

--- src/main/java/com/parfenov/todo/http/controller/TaskController.java
+++ src/main/java/com/parfenov/todo/http/controller/TaskController.java
@@ -7,7 +7,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
--- src/main/java/com/parfenov/todo/http/rest/UserRestController.java
+++ src/main/java/com/parfenov/todo/http/rest/UserRestController.java
@@ -5,7 +5,12 @@ import com.parfenov.todo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
